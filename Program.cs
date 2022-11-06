11888181d83b7ed3b244c09207885a875437fe71
e.polovinkin <polkill@list.ru>
2022-11-06 21:52:42 +0300

    sem2hw

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// Console.Write("введите трехзначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// string stringNumber = Convert.ToString(number);
+// Console.WriteLine("вторая цифра числа = "+stringNumber[1]);
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// Console.Write("введите число: ");
+
+// int number = Convert.ToInt32(Console.ReadLine());
+// string numberText = Convert.ToString(number);
+// if(numberText.Length > 2)
+// {
+//     Console.WriteLine("третья цифра числа = "+numberText[2]);
+// }
+// else
+// {
+//     Console.WriteLine("третьей цифры нет");
+// }
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// Console.Write("введите цифру, что соответствует дню недели: ");
+// int daynumber = Convert.ToInt32(Console.ReadLine());
+
+// if (daynumber == 6 || daynumber == 7)
+// {
+//     Console.WriteLine("выходной день");
+// }
+// else if(daynumber < 1 || daynumber > 7)
+// {
+//     Console.WriteLine("ведите число от 1-7");
+
+// }
+// else
+// {
+//     Console.WriteLine("рабочий день");
+// }
